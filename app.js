4ec53ebbdd61110f20345bec4885ab43d5ce4f79
Daniel Shields <88106441+DukeDan1@users.noreply.github.com>
2022-02-11 23:01:04 +0000

    added news API endpoint

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const request = require('request');
 const mysql = require('mysql');
 const express = require('express');
 const bodyParser = require("body-parser");
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI(process.env.NEWS);
+const querystring = require('querystring');
 const app = express();
 app.disable('x-powered-by');
 app.set('trust proxy',true); 
@@ -19,7 +22,14 @@ app.use(bodyParser.raw({ verify: rawBodySaver, type: '*/*', limit:'50mb' }));
 app.all('/', (req, res) => res.send('Hello world'));
 
 app.listen(8000, '0.0.0.0', () => {
-    console.log(`Webserver running on port 7000.`);
+    console.log(`Webserver running on port 8000.`);
 });
 
-console.log(process.env.NEWS);
+app.get('/news', (req, res) => {
+    if(!req.query.country) res.status(400).json({success:false, reason:"No country provided."});
+    newsapi.v2.topHeadlines({
+        country: req.query.country
+      }).then(response => {
+        res.json({success:true, data: response});
+      });
+});
